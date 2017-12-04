b3f99d4ff899014185c96ab2a3e335f11c126f75
Andrey885 <33977636+Andrey885@users.noreply.github.com>
2017-12-05 00:44:57 +0300

    Add files via upload

---
+++ emotion_recognizer.py
@@ -0,0 +1,29 @@
+import cv2
+
+import os
+
+import numpy as np
+
+
+NNET_FOLDER = os.path.join("models", "emotion_recognition")
+ #place the adress to file with caffe
+
+
+nnet = cv2.dnn.readNetFromTensorflow(os.path.join(NNET_FOLDER, 'emotion_recognition.pb'))
+
+emotions = ["angry", "disgusted", "fearful", "happy", "sad", "surprised", "neutral"]
+
+
+def getEmotion(img):
+    
+	img = cv2.resize(img, (48, 48))
+    
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+	img = cv2.normalize(img, img, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    
+	blob = cv2.dnn.blobFromImage(img)
+    
+	nnet.setInput(blob)
+    
+	return emotions[ np.argmax(nnet.forward()[0]) ]
---
+++ face detector (main).py
@@ -0,0 +1,92 @@
+import numpy as np
+
+import cv2 as cv
+
+import os
+
+import emotion_recognizer as er
+
+
+NNET_FOLDER = os.path.join("models", "face_detection")#place the adress to caffe file
+CONFIDENCE_THRESHOLD = 0.2
+
+
+net=cv.dnn.readNetFromCaffe(os.path.join(NNET_FOLDER, "face_detector_config.prototxt"), os.path.join(NNET_FOLDER, "face_detector_weights.caffemodel"))
+
+
+def detectFaces(frame):
+    
+	blob=cv.dnn.blobFromImage(frame, 1.0, (300,300), (104,177,123),False, False)
+    
+        net.setInput(blob)
+    
+	out = net.forward()
+    
+
+        N = out.shape[2]
+    
+	bboxes = []
+    
+	for i in range (N):
+        
+		vec = out[0, 0 , i]
+        
+		#print vec.shape
+        
+		confidence = vec[2]
+        
+		left = vec[3]
+        
+		top = vec[4]
+        
+		right = vec [5]
+        
+		bottom = vec[6]
+        
+		#print confidence, left, right, bottom
+        
+		row = frame.shape[0]
+        
+		column = frame.shape[1]
+        
+		left = left*column
+        
+		left = int(left)
+        
+		right = right*column
+        
+		right = int(right)
+       
+		top = top*row
+        
+		top = int(top)
+        
+		bottom = bottom*row
+        
+		bottom = int(bottom)
+        
+		if confidence > CONFIDENCE_THRESHOLD:
+            
+			cv.rectangle(frame, (left, top), (right, bottom), (0, 255, 255))
+            
+		bboxes.append([left, top, right, bottom])
+    
+	return bboxes
+
+if
+cap = cv.VideoCapture(0)  # Open camera stream.
+    
+while cv.waitKey(1) < 0:
+        # Read frame from camera
+        
+has_frame, frame = cap.read()
+        
+if not has_frame:
+            
+	break
+        
+detectFaces(frame)
+        
+print(er.getEmotion(frame))
+        
+cv.imshow('Frame', frame)  # Display output
